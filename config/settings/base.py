e0efefacf5061c19c3051ecc83fbe685083cd151
masakos <masako.4545.715@gmail.com>
2018-10-07 11:22:06 +0900

    fix ci

--- config/settings/base.py
+++ config/settings/base.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('APP_KEY')
+#SECRET_KEY = env('APP_KEY')
+SECRET_KEY = 'fc3(hb8s1(#^3b9+ghzqzz79@m3f1xbc%#a^988u%$%$&o%)8p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
